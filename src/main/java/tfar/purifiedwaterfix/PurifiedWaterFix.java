b74313b4e6f9ac076cfc37f28975f250e77c5671
ifich <ira.fich@gmail.com>
2024-02-03 22:53:18 -0500

    extract some strings and resources into constants

--- src/main/java/tfar/purifiedwaterfix/PurifiedWaterFix.java
+++ src/main/java/tfar/purifiedwaterfix/PurifiedWaterFix.java
@@ -11,14 +11,23 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = PurifiedWaterFix.MOD_ID)
 @Mod.EventBusSubscriber
 public class PurifiedWaterFix {
+    
+    // mod names
     public static final String MOD_ID = "purifiedwaterfix";
-
     public static final String TAN = "toughasnails";
 
-    public static final ResourceLocation PURIFIED_WATER = new ResourceLocation(TAN,"purified_water_bottle");
-    public static final ResourceLocation CANTEEN = new ResourceLocation(TAN,"canteen");
+    // resources
+    public static final String PURIFIED_WATER = "purified_water";
+    private static final String PURIFIED_WATER_BOTTLE = "purified_water_bottle";
+    public static final ResourceLocation PURIFIED_WATER_TAN = new ResourceLocation(TAN, PURIFIED_WATER_BOTTLE);
+    public static final ResourceLocation PURIFIED_WATER_WRAPPER = new ResourceLocation(MOD_ID, PURIFIED_WATER_BOTTLE);
 
+    private static final String CANTEEN = "canteen";
+    public static final ResourceLocation CANTEEN_TAN = new ResourceLocation(TAN, CANTEEN);
+    public static final ResourceLocation CANTEEN_WRAPPER = new ResourceLocation(MOD_ID, CANTEEN);
 
+    public static final ResourceLocation GLASS_BOTTLE = new ResourceLocation(MOD_ID, "glass_bottle");
+    
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
     }
@@ -27,12 +36,14 @@ public class PurifiedWaterFix {
     public static void attachCaps(AttachCapabilitiesEvent<ItemStack> event) {
         ItemStack stack = event.getObject();
         ResourceLocation registryName = stack.getItem().getRegistryName();
-        if (PURIFIED_WATER.equals(registryName)) {
-            event.addCapability(new ResourceLocation(MOD_ID,"purified_water_bottle"),new FilledFluidBottleWrapper(stack));
-        } else if (CANTEEN.equals(registryName)) {
-            event.addCapability(new ResourceLocation(MOD_ID,"canteen"),new CanteenWrapper(stack));
+        if (PURIFIED_WATER_TAN.equals(registryName)) {
+            event.addCapability(PURIFIED_WATER_WRAPPER, new FilledFluidBottleWrapper(stack));
+            
+        } else if (CANTEEN_TAN.equals(registryName)) {
+            event.addCapability(CANTEEN_WRAPPER, new CanteenWrapper(stack));
+            
         } else if (stack.getItem() == Items.GLASS_BOTTLE) {
-            event.addCapability(new ResourceLocation(MOD_ID,"glass_bottle"),new EmptyBottleWrapper(stack));
+            event.addCapability(GLASS_BOTTLE, new EmptyBottleWrapper(stack));
         }
     }
 }
